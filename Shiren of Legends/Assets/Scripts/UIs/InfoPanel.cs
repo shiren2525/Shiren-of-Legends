abc596b00d8ec1345d792e39edb56e1a9ac36d01
shiren2525 <furainoshiren2525@gmail.com>
2020-12-03 22:51:50 +0900

    fixed cardData fixed infoPanel

--- Shiren of Legends/Assets/Scripts/UIs/InfoPanel.cs
+++ Shiren of Legends/Assets/Scripts/UIs/InfoPanel.cs
@@ -14,7 +14,7 @@ public class InfoPanel : MonoBehaviour
         for (int i = 0; i < 8; i++)
         {
             Texts[i].text =
-                CardDate.KeyValuesName[i] + ":HP" + CardDate.KeyValuesAD[i].ToString() + "/AD" + CardDate.KeyValuesHP[i].ToString() + "\n" +
+                CardDate.KeyValuesName[i] + ":HP" + CardDate.KeyValuesHP[i].ToString() + "/AD" + CardDate.KeyValuesAD[i].ToString() + "\n" +
                 spliteText[i].Replace("ad", (CardDate.KeyValuesAD[i] * CardDate.KeyValuesRatio[i]).ToString())
                              .Replace("hp", CardDate.KeyValuesHP[i].ToString());
         }
