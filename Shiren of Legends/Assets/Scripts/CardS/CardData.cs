917b39048649602289a9b409b44f5ffae24cde42
shiren2525 <furainoshiren2525@gmail.com>
2020-12-14 14:14:55 +0900

    fixed asset

--- Shiren of Legends/Assets/Scripts/CardS/CardData.cs
+++ Shiren of Legends/Assets/Scripts/CardS/CardData.cs
@@ -18,7 +18,7 @@ public class CardData : MonoBehaviour
            new CardParameter(){ ID=4, Name="Shyvana",   HP=3, AD=3, Ratio=0.2f },
            new CardParameter(){ ID=2, Name="Karthus",   HP=5, AD=5, Ratio=2.0f },
            new CardParameter(){ ID=6, Name="Vladimir",  HP=6, AD=10, Ratio=0.2f },
-           new CardParameter(){ ID=5, Name="Soraka",    HP=4, AD=2, Ratio=1.0f },
+           new CardParameter(){ ID=5, Name="Soraka",    HP=3, AD=2, Ratio=1.0f },
            new CardParameter(){ ID=3, Name="Sena",      HP=4, AD=2, Ratio=0.5f },
            new CardParameter(){ ID=7, Name="Yasuo",     HP=10, AD=10, Ratio=1.0f },
         };
